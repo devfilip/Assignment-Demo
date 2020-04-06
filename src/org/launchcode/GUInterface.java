59f4dad3cc716b1280afaec2afffb50b30902e44
devfilip <txficvk@gmail.com>
2020-04-06 22:31:19 +0200

    Adding comments

--- src/org/launchcode/GUInterface.java
+++ src/org/launchcode/GUInterface.java
@@ -9,19 +9,19 @@ public class GUInterface {
 
         JPanel myPanel = new JPanel();
 
-        JButton submit = new JButton("Submit"); //submit button object
+        JButton submit = new JButton("Submit");                // submit button object
 
         JTextField nameField = new JTextField(10);
         JLabel nameLabel = new JLabel("Name: ");
-        myPanel.add(nameLabel); //just a label with a "name" text;
-        myPanel.add(nameField); // name to fill with student's name
-        myPanel.add(submit); // adding submit button to my panel
+        myPanel.add(nameLabel);                                     // just a label with a "name" text;
+        myPanel.add(nameField);                                     // adding a text field to fill with student's name
+        myPanel.add(submit);                                        // adding submit button to my panel
 
 
-        mainFrame.setSize(500,500);
-        mainFrame.setContentPane(myPanel);
+        mainFrame.setSize(500,500);                    // setting window size
+        mainFrame.setContentPane(myPanel);                          // adding a panel onto mainFrame
         mainFrame.setVisible(true);
-        mainFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        mainFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);   // window closing when click on Exit button
     }
 
 }
--- src/org/launchcode/StudentsMajor.java
+++ src/org/launchcode/StudentsMajor.java
@@ -1,9 +1,9 @@
 package org.launchcode;
 
 public enum StudentsMajor {
-    IT, //informatyka
-    AW, // architektura wnetrz
-    GRAPH, //Grafika
-    CULT, //Kulturoznawstwo
-    IM, //Zarzadzanie informacja
+    IT,     // Informatyka
+    AW,     // Architektura wnetrz
+    GRAPH,  // Grafika
+    CULT,   // Kulturoznawstwo
+    IM,     // Zarzadzanie informacja
 }
