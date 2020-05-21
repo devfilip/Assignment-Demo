f81f994b202a51dabc10dfd253b7d3adcbb5786d
devfilip <txficvk@gmail.com>
2020-05-21 21:53:17 +0200

    All in one

--- src/org/launchcode/GUInterface.java
+++ src/org/launchcode/GUInterface.java
@@ -1,14 +1,7 @@
 package org.launchcode;
 
 import javax.swing.*;
-import javax.swing.SpringLayout;
-import javax.swing.border.Border;
-import javax.tools.Tool;
 import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import java.sql.Array;
-import java.util.ArrayList;
 
 public class GUInterface{
 
@@ -16,137 +9,75 @@ public class GUInterface{
     public static void createAndShowGui() {
         JFrame mainFrame = new JFrame("First assignment");
         JPanel mainPanel = new JPanel(new BorderLayout());
-        JTextArea studentTextArea = new JTextArea();
-        JLabel successDel = new JLabel("Student deleted successfully!");
-        JLabel success1 = new JLabel("Student added successfully!");
-        ArrayList<Student> studentList;
-
-
+        JPanel menu = new JPanel(new FlowLayout());
+        JPanel addForm = new JPanel(new GridLayout(0,2,20,10));
+        
         JButton addStudent = new JButton("Add student");
         JButton showStudents = new JButton("Display students list");
         JButton modifyStudent = new JButton("Edit student");
-        JButton deleteStudent = new JButton("Delete all");
+        JButton deleteStudent = new JButton("Delete student");
         JButton searchStudent = new JButton("Search");
 
-        JButton submit = new JButton("Submit");
+        JLabel nameLabel = new JLabel("Name: ");
+        JLabel surnameLabel = new JLabel("Surname: ");
+        JLabel majorLabel = new JLabel("Major: ");
+        JLabel yearLabel = new JLabel("Year of Study: ");
+        JLabel avgLabel = new JLabel("Avg. grade: ");
+        JLabel empty = new JLabel("");
 
-        JPanel butPanel = new JPanel();
-        butPanel.setLayout(new BoxLayout(butPanel,BoxLayout.PAGE_AXIS));
-        butPanel.setBackground(Color.darkGray);
-        mainPanel.add(butPanel, BorderLayout.LINE_START);
+        JTextField nameField = new JTextField(15);
+        JTextField surnameField = new JTextField(15);
+        JComboBox majorCombo = new JComboBox(StudentsMajor.values());
+        JTextField yearField = new JTextField(15);
+        JTextField avgField = new JTextField(15);
+
+        JButton apply = new JButton("Submit");
+
         Font butFont = new Font("Helvetica",Font.CENTER_BASELINE, 13);
 
+        menu.setBackground(Color.darkGray);
         addStudent.setFont(butFont);
         showStudents.setFont(butFont);
         modifyStudent.setFont(butFont);
         deleteStudent.setFont(butFont);
         searchStudent.setFont(butFont);
 
-        butPanel.add(addStudent);
-        butPanel.add(Box.createRigidArea(new Dimension(5,5)));
-        butPanel.add(showStudents);
-        butPanel.add(Box.createRigidArea(new Dimension(5,5)));
-        butPanel.add(modifyStudent);
-        butPanel.add(Box.createRigidArea (new Dimension(5,5)));
-        butPanel.add(deleteStudent);
-        butPanel.add(Box.createRigidArea(new Dimension(5,5)));
-        butPanel.add(searchStudent);
-        butPanel.add(Box.createRigidArea(new Dimension(5,5)));
+        addForm.setBackground(Color.darkGray);
+        addForm.setForeground(Color.WHITE);
 
-        JPanel formPanel = new JPanel(new SpringLayout());
-        formPanel.setBackground(Color.darkGray);
+        menu.add(addStudent);
+        menu.add(showStudents);
+        menu.add(modifyStudent);
+        menu.add(deleteStudent);
+        menu.add(searchStudent);
+        menu.setComponentOrientation(ComponentOrientation.LEFT_TO_RIGHT);
 
-        studentList = new ArrayList<Student>();
-        String[] labels = {"Name: ", "Surname: ", "Major: ", "Year of Study: ", "Average grade: "};
+        mainPanel.add(menu, BorderLayout.NORTH);
+        addStudent.addActionListener(e -> {
+            addForm.add(nameLabel);
+            addForm.add(nameField);
+            addForm.add(surnameLabel);
+            addForm.add(surnameField);
+            addForm.add(majorLabel);
+            addForm.add(majorCombo);
+            addForm.add(yearLabel);
+            addForm.add(yearField);
+            addForm.add(avgLabel);
+            addForm.add(avgField);
+            addForm.add(empty);
+            addForm.add(apply);
 
-        int numPairs = labels.length;
-        for (int i = 0; i < numPairs; i++) {
-            JLabel l = new JLabel(labels[i], JLabel.TRAILING);
-            l.setForeground(Color.white);
-            formPanel.add(l);
-            JTextField textField = new JTextField(15);
-            l.setLabelFor(textField);
-            formPanel.add(textField);
-        }
-
-        SpringUtilities.makeCompactGrid(formPanel,
-                numPairs, 2, //rows, cols
-                3, 3,        //initX, initY
-                3, 3);       //xPad, yPad
-        mainPanel.add(formPanel, BorderLayout.CENTER);
-        mainPanel.add(submit, BorderLayout.SOUTH);
-        submit.addActionListener(e -> {
-            JOptionPane.showMessageDialog(mainFrame, "Student added successfully!", "Good Job",
-                    JOptionPane.INFORMATION_MESSAGE );
+            mainPanel.add(addForm,BorderLayout.CENTER);
+            mainPanel.revalidate();
         });
 
-        mainPanel.add(success1, BorderLayout.LINE_END);
-        success1.setVisible(false);
-        success1.setFont(new Font("Helvetica", Font.ITALIC, 14));
-        success1.setForeground(Color.yellow);
-
-//       addStudent.addActionListener(new ActionListener() {
-//            @Override
-//            public void actionPerformed(ActionEvent e) {
-//                if (e.getActionCommand().equals("add")) {
-//                    success1.setVisible(true);
-//
-//                    StudentsMajor selectedMajor = (StudentsMajor)majorCombo.getSelectedItem();
-//                    int selectedYear = (int) yearCombo.getSelectedItem();
-//                    double avg = Double.valueOf(averageField.getText());
-//
-//
-//                    Student studentData = new Student(nameField.getText(), surnameField.getText(), selectedMajor, selectedYear,avg);
-//                    studentList.add(studentData);
-//                    studentTextArea.setText("");
-//                    studentTextArea.setVisible(false);
-//                }
-//            }
-//        });
-
-
-//        menu.showStudents.addActionListener(new ActionListener() {
-//            @Override
-//            public void actionPerformed(ActionEvent e) {
-//
-//                menu.success1.setVisible(false);
-//                successDel.setVisible(false);
-//                studentTextArea.setVisible(true);
-//                studentTextArea.setBounds(10,250,980,320);
-//                studentTextArea.setEditable(false);
-//                studentTextArea.setBackground(Color.lightGray);
-//                mainPanel.add(studentTextArea);
-//                mainPanel.repaint();
-//
-//                    for (Student stud : menu.studentList) {
-//                            studentTextArea.append(stud + "\n");
-//                    }
-//
-//
-//            }
-//        });
-//
-//        menu.modifyStudent.addActionListener(new ActionListener() {
-//            @Override
-//            public void actionPerformed(ActionEvent e) {
-//
-//            }
-//        });
-//        menu.deleteStudent.addActionListener(new ActionListener() {
-//            @Override
-//            public void actionPerformed(ActionEvent e) {
-//                menu.studentList.removeAll(menu.studentList);
-//                studentTextArea.setText("");
-//                studentTextArea.setVisible(false);
-//                successDel.setVisible(true);
-//                successDel.setBounds(730, 80, 200, 20);
-//                successDel.setFont(new Font("Helvetica", Font.ITALIC, 14));
-//                successDel.setForeground(Color.yellow);
-//                mainPanel.add(successDel);
-//                mainPanel.repaint();
-//            }
-//        });
-
+        apply.addActionListener(e -> {
+            Student student = new Student(nameField.getText(),
+                    surnameField.getText(),
+                    (StudentsMajor)majorCombo.getSelectedItem(),
+                    Integer.parseInt(yearField.getText()),
+                    Double.parseDouble(avgField.getText()));
+        });
 
         Dimension screenDim = Toolkit.getDefaultToolkit().getScreenSize();      // setting window size
         mainFrame.setSize(screenDim.width / 2,screenDim.height / 2);
